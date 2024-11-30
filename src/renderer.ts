1a36f9beaa244a10ad9fc8b61ae2ce5fcdd69b4e
Jack Ashton <jack.ashton.nz@gmail.com>
2024-11-30 18:47:56 +1300

    Fix rendering

--- src/renderer.ts
+++ src/renderer.ts
@@ -11,11 +11,10 @@ interface Renderer {
 
 class TopDownRenderer implements Renderer {
   constructor(
-    public gl: WebGL2RenderingContext,
-    public program: WebGLProgram,
-    public width: number,
-    public height: number,
-    public screenHeight: number,
+    private gl: WebGL2RenderingContext,
+    private program: WebGLProgram,
+    private width: number,
+    private height: number,
   ) {}
 
   render(scene: Scene): void {
@@ -29,6 +28,9 @@ class TopDownRenderer implements Renderer {
   }
 
   private drawPlayer(player: Player): void {
+    const color = [0.0, 1.0, 1.0, 1.0];
+    const showDirection = true;
+
     const positionBuffer = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
     this.gl.bufferData(
@@ -110,30 +112,33 @@ class FirstPersonRenderer implements Renderer {
 
   render(scene: Scene): void {
     // TODO cache these so we don't have to find them in the scene every time
-    let map: Map;
-    let player: Player;
+    let map: Map | undefined;
+    let player: Player | undefined;
     for (const obj of scene.objects) {
       if (obj instanceof Player) {
         player = obj;
       } else if (obj instanceof Map) {
         map = obj;
       }
+
       if (player && map) break;
     }
+
+    if (!player || !map) throw new Error('Player or Map is not defined');
 
     const rayAngleDelta = (2 * Math.PI) / 360;
     const fov = 60;
 
     const textureSize = 32;
 
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    const positionBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
 
-    const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+    const positionAttributeLocation = this.gl.getAttribLocation(this.program, 'a_position');
+    this.gl.enableVertexAttribArray(positionAttributeLocation);
+    this.gl.vertexAttribPointer(positionAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
 
-    const colorLocation = gl.getUniformLocation(program, 'u_color');
+    const colorLocation = this.gl.getUniformLocation(this.program, 'u_color');
 
     const horizontalHitWallColor = [0.9, 0.0, 0.0, 1.0];
     const verticalHitWallColor = [0.7, 0.0, 0.0, 1.0];
@@ -164,15 +169,15 @@ class FirstPersonRenderer implements Renderer {
       // Horizontal line checks
       if (rayAngle < Math.PI) {
         // looking up
-        rayPosition.y = Math.floor(player.position.y / this.mapS) * this.mapS - 0.0001;
+        rayPosition.y = Math.floor(player.position.y / map.mapS) * map.mapS - 0.0001;
         rayPosition.x = (player.position.y - rayPosition.y) * aTan + player.position.x;
-        offset.y = -this.mapS;
+        offset.y = -map.mapS;
         offset.x = -offset.y * aTan;
       } else if (rayAngle > Math.PI) {
         // looking down
-        rayPosition.y = Math.floor(player.position.y / this.mapS) * this.mapS + this.mapS;
+        rayPosition.y = Math.floor(player.position.y / map.mapS) * map.mapS + map.mapS;
         rayPosition.x = (player.position.y - rayPosition.y) * aTan + player.position.x;
-        offset.y = this.mapS;
+        offset.y = map.mapS;
         offset.x = -offset.y * aTan;
       } else {
         // looking straight left or right
@@ -183,15 +188,15 @@ class FirstPersonRenderer implements Renderer {
 
       // Perform raycasting for horizontal lines
       while (dof < maxDof) {
-        const mx = Math.floor(rayPosition.x / this.mapS);
-        const my = Math.floor(rayPosition.y / this.mapS);
-        const mp = my * this.mapX + mx;
+        const mx = Math.floor(rayPosition.x / map.mapS);
+        const my = Math.floor(rayPosition.y / map.mapS);
+        const mp = my * map.mapX + mx;
         // hit
-        if (mp < this.mapX * this.mapY && this.mapW[mp] > 0) {
+        if (mp < map.mapX * map.mapY && map.mapW[mp] > 0) {
           horizontalRayPosition.x = rayPosition.x;
           horizontalRayPosition.y = rayPosition.y;
           distanceHorizontal = player.position.distance(horizontalRayPosition);
-          horizontalMapTextureIndex = this.mapW[mp] - 1;
+          horizontalMapTextureIndex = map.mapW[mp] - 1;
           dof = maxDof;
         } else {
           // go to next line
@@ -205,9 +210,9 @@ class FirstPersonRenderer implements Renderer {
       // Vertical line checks
       if (rayAngle < Math.PI / 2 || rayAngle > (3 * Math.PI) / 2) {
         // looking right
-        rayPosition.x = Math.floor(player.position.x / this.mapS) * this.mapS + this.mapS;
+        rayPosition.x = Math.floor(player.position.x / map.mapS) * map.mapS + map.mapS;
         rayPosition.y = (player.position.x - rayPosition.x) * tan + player.position.y;
-        offset.x = this.mapS;
+        offset.x = map.mapS;
         offset.y = -offset.x * tan;
       } else if (rayAngle === Math.PI / 2 || rayAngle === (3 * Math.PI) / 2) {
         // looking straight up or down
@@ -216,23 +221,23 @@ class FirstPersonRenderer implements Renderer {
         dof = maxDof;
       } else {
         // looking left
-        rayPosition.x = Math.floor(player.position.x / this.mapS) * this.mapS - 0.0001;
+        rayPosition.x = Math.floor(player.position.x / map.mapS) * map.mapS - 0.0001;
         rayPosition.y = (player.position.x - rayPosition.x) * tan + player.position.y;
-        offset.x = -this.mapS;
+        offset.x = -map.mapS;
         offset.y = -offset.x * tan;
       }
 
       // Perform raycasting for vertical lines
       while (dof < maxDof) {
-        const mx = Math.floor(rayPosition.x / this.mapS);
-        const my = Math.floor(rayPosition.y / this.mapS);
-        const mp = my * this.mapX + mx;
+        const mx = Math.floor(rayPosition.x / map.mapS);
+        const my = Math.floor(rayPosition.y / map.mapS);
+        const mp = my * map.mapX + mx;
         // hit
-        if (mp < this.mapX * this.mapY && this.mapW[mp] > 0) {
+        if (mp < map.mapX * map.mapY && map.mapW[mp] > 0) {
           verticalRayPosition.x = rayPosition.x;
           verticalRayPosition.y = rayPosition.y;
           distanceVertical = player.position.distance(verticalRayPosition);
-          verticalMapTextureIndex = this.mapW[mp] - 1;
+          verticalMapTextureIndex = map.mapW[mp] - 1;
           dof = maxDof;
         } else {
           // go to next line
@@ -263,32 +268,32 @@ class FirstPersonRenderer implements Renderer {
         wallColor = horizontalHitWallColor;
       }
 
-      gl.uniform4fv(colorLocation, wallColor);
-      gl.bufferData(
-        gl.ARRAY_BUFFER,
+      this.gl.uniform4fv(colorLocation, wallColor);
+      this.gl.bufferData(
+        this.gl.ARRAY_BUFFER,
         new Float32Array([
-          ...toClipSpace(width, height, player.position.x, player.position.y),
-          ...toClipSpace(width, height, rayPosition.x, rayPosition.y),
+          ...toClipSpace(this.width, this.height, player.position.x, player.position.y),
+          ...toClipSpace(this.width, this.height, rayPosition.x, rayPosition.y),
         ]),
-        gl.STATIC_DRAW,
+        this.gl.STATIC_DRAW,
       );
-      gl.drawArrays(gl.LINES, 0, 2);
+      this.gl.drawArrays(this.gl.LINES, 0, 2);
 
       // draw walls
       // fix fisheye only on horizontal distance
       const rayAngleFixed = Math.cos(normalizeAngle(player.angle - rayAngle));
       if (distance === distanceHorizontal) distance = distance * rayAngleFixed;
 
-      let lineHeight = (this.mapS * screenHeight) / distance;
+      let lineHeight = (map.mapS * this.screenHeight) / distance;
       const textureYStep = textureSize / lineHeight;
       let textureYOffset = 0;
 
-      if (lineHeight > screenHeight) {
-        textureYOffset = (lineHeight - screenHeight) / 2;
-        lineHeight = screenHeight;
+      if (lineHeight > this.screenHeight) {
+        textureYOffset = (lineHeight - this.screenHeight) / 2;
+        lineHeight = this.screenHeight;
       }
 
-      const lineOffset = screenHeight / 2 - (lineHeight >> 1);
+      const lineOffset = this.screenHeight / 2 - (lineHeight >> 1);
 
       let textureY = textureYOffset * textureYStep;
 
@@ -309,71 +314,71 @@ class FirstPersonRenderer implements Renderer {
 
       for (let y = 0; y < lineHeight; y++) {
         const pixelIndex = (Math.trunc(textureY) * textureSize + Math.trunc(textureX)) * 3;
-        const red = this.textures[horizontalMapTextureIndex][pixelIndex] / 255;
-        const green = this.textures[horizontalMapTextureIndex][pixelIndex + 1] / 255;
-        const blue = this.textures[horizontalMapTextureIndex][pixelIndex + 2] / 255;
-        gl.uniform4fv(colorLocation, [red * shade, green * shade, blue * shade, 1.0]);
-        gl.bufferData(
-          gl.ARRAY_BUFFER,
+        const red = map.textures[horizontalMapTextureIndex][pixelIndex] / 255;
+        const green = map.textures[horizontalMapTextureIndex][pixelIndex + 1] / 255;
+        const blue = map.textures[horizontalMapTextureIndex][pixelIndex + 2] / 255;
+        this.gl.uniform4fv(colorLocation, [red * shade, green * shade, blue * shade, 1.0]);
+        this.gl.bufferData(
+          this.gl.ARRAY_BUFFER,
           new Float32Array([
-            ...toClipSpace(width, height, r * 8 + 530, y + lineOffset),
-            ...toClipSpace(width, height, r * 8 + 530, y + lineOffset + 8),
-            ...toClipSpace(width, height, r * 8 + 530 + 8, y + lineOffset + 8),
-            ...toClipSpace(width, height, r * 8 + 530 + 8, y + lineOffset),
+            ...toClipSpace(this.width, this.height, r * 8 + 530, y + lineOffset),
+            ...toClipSpace(this.width, this.height, r * 8 + 530, y + lineOffset + 8),
+            ...toClipSpace(this.width, this.height, r * 8 + 530 + 8, y + lineOffset + 8),
+            ...toClipSpace(this.width, this.height, r * 8 + 530 + 8, y + lineOffset),
           ]),
-          gl.STATIC_DRAW,
+          this.gl.STATIC_DRAW,
         );
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, 4);
         textureY += textureYStep;
       }
 
       // draw floors and ceilings
-      for (let y = lineOffset + lineHeight; y < screenHeight; y++) {
+      for (let y = lineOffset + lineHeight; y < this.screenHeight; y++) {
         // floors
-        const dy = y - screenHeight / 2;
+        const dy = y - this.screenHeight / 2;
         const textureX = player.position.x / 2 + (Math.cos(rayAngle) * 158 * textureSize) / dy / rayAngleFixed;
         const textureY = player.position.y / 2 - (Math.sin(rayAngle) * 158 * textureSize) / dy / rayAngleFixed;
-        let mp = this.mapF[Math.floor(textureY / textureSize) * this.mapX + Math.floor(textureX / textureSize)] - 1;
+        let mp = map.mapF[Math.floor(textureY / textureSize) * map.mapX + Math.floor(textureX / textureSize)] - 1;
         let pixelIndex =
           ((Math.floor(textureY) & (textureSize - 1)) * textureSize + (Math.floor(textureX) & (textureSize - 1))) * 3 +
           mp * 3;
-        let red = this.textures[mp][pixelIndex] / 255;
-        let green = this.textures[mp][pixelIndex + 1] / 255;
-        let blue = this.textures[mp][pixelIndex + 2] / 255;
-        gl.uniform4fv(colorLocation, [red, green, blue, 1.0]);
-        gl.bufferData(
-          gl.ARRAY_BUFFER,
+        let red = map.textures[mp][pixelIndex] / 255;
+        let green = map.textures[mp][pixelIndex + 1] / 255;
+        let blue = map.textures[mp][pixelIndex + 2] / 255;
+        this.gl.uniform4fv(colorLocation, [red, green, blue, 1.0]);
+        this.gl.bufferData(
+          this.gl.ARRAY_BUFFER,
           new Float32Array([
-            ...toClipSpace(width, height, r * 8 + 530, y),
-            ...toClipSpace(width, height, r * 8 + 530, y + 8),
-            ...toClipSpace(width, height, r * 8 + 530 + 8, y + 8),
-            ...toClipSpace(width, height, r * 8 + 530 + 8, y),
+            ...toClipSpace(this.width, this.height, r * 8 + 530, y),
+            ...toClipSpace(this.width, this.height, r * 8 + 530, y + 8),
+            ...toClipSpace(this.width, this.height, r * 8 + 530 + 8, y + 8),
+            ...toClipSpace(this.width, this.height, r * 8 + 530 + 8, y),
           ]),
-          gl.STATIC_DRAW,
+          this.gl.STATIC_DRAW,
         );
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, 4);
 
         // ceilings
-        mp = this.mapC[Math.floor(textureY / textureSize) * this.mapX + Math.floor(textureX / textureSize)] - 1;
+        mp = map.mapC[Math.floor(textureY / textureSize) * map.mapX + Math.floor(textureX / textureSize)] - 1;
         pixelIndex =
           ((Math.floor(textureY) & (textureSize - 1)) * textureSize + (Math.floor(textureX) & (textureSize - 1))) * 3 +
           mp * 3;
-        red = this.textures[mp][pixelIndex] / 255;
-        green = this.textures[mp][pixelIndex + 1] / 255;
-        blue = this.textures[mp][pixelIndex + 2] / 255;
-        gl.uniform4fv(colorLocation, [red, green, blue, 1.0]);
-        gl.uniform4fv(colorLocation, [red, green, blue, 1]);
-        gl.bufferData(
-          gl.ARRAY_BUFFER,
+        red = map.textures[mp][pixelIndex] / 255;
+        green = map.textures[mp][pixelIndex + 1] / 255;
+        blue = map.textures[mp][pixelIndex + 2] / 255;
+        this.gl.uniform4fv(colorLocation, [red, green, blue, 1.0]);
+        this.gl.uniform4fv(colorLocation, [red, green, blue, 1]);
+        this.gl.bufferData(
+          this.gl.ARRAY_BUFFER,
           new Float32Array([
-            ...toClipSpace(width, height, r * 8 + 530, screenHeight - y),
-            ...toClipSpace(width, height, r * 8 + 530, screenHeight - y + 8),
-            ...toClipSpace(width, height, r * 8 + 530 + 8, screenHeight - y + 8),
-            ...toClipSpace(width, height, r * 8 + 530 + 8, screenHeight - y),
+            ...toClipSpace(this.width, this.height, r * 8 + 530, this.screenHeight - y),
+            ...toClipSpace(this.width, this.height, r * 8 + 530, this.screenHeight - y + 8),
+            ...toClipSpace(this.width, this.height, r * 8 + 530 + 8, this.screenHeight - y + 8),
+            ...toClipSpace(this.width, this.height, r * 8 + 530 + 8, this.screenHeight - y),
           ]),
-          gl.STATIC_DRAW,
+          this.gl.STATIC_DRAW,
         );
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, 4);
       }
 
       // Move to next ray
